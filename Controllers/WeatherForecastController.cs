d8c69fc3ede265adcd983330ecf8f16ed950490a
keshavpratap-singh <keshav.pratap-singh@nagarro.com>
2023-01-05 14:21:08 +0530

    second commit

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace JenkinsProject.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Hoot", "Swelltering", "Sccorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
---
+++ model/Class.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JenkinsProject.model
+{
+    public class Class
+    {
+        private int number1;
+
+        public int Getnumber()
+        {
+            return number1;
+        }
+
+        public void Setnumber(int value)
+        {
+            number1 = value;
+        }
+    }
+}
